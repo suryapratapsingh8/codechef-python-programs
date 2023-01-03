2605e9653d43629cfeb284d3659da3cbc1e93e2f
Surya Pratap Singh <117995172+suryapratapsingh8@users.noreply.github.com>
2023-01-03 10:06:21 +0530

    Add files via upload

---
+++ CHEFROUT.py
@@ -0,0 +1,13 @@
+# chef and his daily routine 
+for _ in range(int(input())):
+    s=input()
+  
+    s = list(s)
+    
+    if (s == sorted(s)):
+        print("yes")
+    else:
+        print("no")
+            
+            
+
