ccc3733e2b95d69fac18698326c5c26549c4bdde
Surya Pratap Singh <117995172+suryapratapsingh8@users.noreply.github.com>
2023-01-13 13:58:42 +0530

    Add files via upload

---
+++ NONADJFLIP.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range(int(input())):
+    n=int(input())
+    s=input()
+    if '11' in s:
+        print('2')
+    elif '1' in s:
+        print('1')
+    else:
+        print('0')
