9c09fe21f27c85bd4d6f8a4c1604f102a5c67f4e
Surya Pratap Singh <117995172+suryapratapsingh8@users.noreply.github.com>
2023-01-14 22:08:22 +0530

    Add files via upload

---
+++ ('First in INDIA').py
@@ -0,0 +1,29 @@
+print('First in INDIA')
+Q=input('Enter your question in lower case:')
+if(Q=='president'):
+    print('Dr Rajendra Prashad')
+    
+elif(Q=='prime minister'):
+    print("Pt Jawahar Lal Nehru")
+elif(Q=='economic census'):
+    print('IN 1977')
+elif(Q=="train"):
+    print("Red hill railway in 1837")
+    print("First passenger train is Sahib, Sindh and Sultan (16 april 1853)")
+elif(Q=='newspaper'):
+    print("Bengal Gazette (1780)")   
+elif(Q=='election'): 
+    print("general election of 1951-1952")
+elif(Q=='bank'): 
+    print("Bank of Hindustan 1770")
+elif(Q=='national park'):
+    print('Jim Corbett National Park 1936') 
+elif(Q=='movie'):
+    print('Raja Harishchandra in 1913')
+    print("movie with sound is ALAM ARA in 1931")
+    print("Movie that shoot abroad is SANGAM in 1964")
+elif(Q=='song'):
+    print("Raga -Jogia by Gauhar Jaan")  
+else:
+    print("check your spelling")
+    print("we are sorry")
