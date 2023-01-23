7d22025cdc0f4f09ebb1884011ddbda990a1a761
Surya Pratap Singh <117995172+suryapratapsingh8@users.noreply.github.com>
2023-01-23 23:09:29 +0530

    Add files via upload

---
+++ FARFROMO.py
@@ -0,0 +1,12 @@
+# cook your dish here
+for t in range(int(input())):
+    x1, y1, x2, y2 = map(int, input().split())
+    alex = pow(x1, 2) + pow(y1, 2)
+    bob = pow(x2, 2) + pow(y2, 2)
+        
+    if(alex > bob):
+        print("Alex")
+    elif(bob > alex):
+        print("Bob")
+    elif(alex == bob):
+        print("Equal")
