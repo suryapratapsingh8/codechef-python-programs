af910be2bf544cabdab78cb83401f9a5b6c336dd
Surya Pratap Singh <117995172+suryapratapsingh8@users.noreply.github.com>
2023-03-05 17:11:34 +0530

    Add files via upload

---
+++ JASSIGNMENTS.py
@@ -0,0 +1,9 @@
+# cook your dish here
+# cook your dish here
+t=int(input())
+for i in range(t):
+        X=int(input())
+        if (X+3)<=10:
+                print("YES")
+        else:
+                print("NO")
