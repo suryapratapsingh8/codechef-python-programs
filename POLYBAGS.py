d95d08a8bed48d181844fe9a6fad1573ae43d668
Surya Pratap Singh <117995172+suryapratapsingh8@users.noreply.github.com>
2023-03-31 23:03:57 +0530

    Add files via upload

---
+++ POLYBAGS.py
@@ -0,0 +1,8 @@
+# cook your dish hereff
+for i in range(int(input())):
+        n=int(input())
+        if n%10==0:
+                print(n//10)
+        else:
+                
+                print((n//10)+1)
