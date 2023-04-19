74adb07dbc4b2b1b00e4b6b630f23641234c72be
Surya Pratap Singh <117995172+suryapratapsingh8@users.noreply.github.com>
2023-04-20 00:50:45 +0530

    Add files via upload

---
+++ AVDSQUARPLZ.py
@@ -0,0 +1,13 @@
+# cook your dish here
+#1 4 9 16 25 36 49
+
+t=int(input())
+for i in range(t):
+    
+   n=int(input())
+   for i in range(1,n+1):
+       print(i,end=' ')
+       
+   print('\n')
+       
+       
