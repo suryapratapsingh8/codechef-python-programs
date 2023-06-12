c073f73d958370cd3a6a6945515a760d83fcec5f
Surya Pratap Singh <117995172+suryapratapsingh8@users.noreply.github.com>
2023-06-12 15:02:32 +0530

    Add files via upload

---
+++ PIZZA_BURGER.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for i in range(int(input())):
+    x,y,z=map(int,input().split())
+    if x>=y:
+        print("PIZZA")
+    elif x>=z:
+        print("BURGER")
+    else:
+        print("NOTHING")
