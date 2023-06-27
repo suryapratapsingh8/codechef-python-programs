6dcaf27e798266ed5bd16c850aa59b63f69ce635
Surya Pratap Singh <117995172+suryapratapsingh8@users.noreply.github.com>
2023-06-27 11:01:52 +0530

    Add files via upload

---
+++ DOREWARD.py
@@ -0,0 +1,12 @@
+# cook your dish here
+for _ in range(int(input())):
+    X=int(input())
+    
+    if X<=3:
+        print("BRONZE")
+        
+    elif 3<X<=6:
+        print("SILVER")
+        
+    else:
+        print("GOLD")
