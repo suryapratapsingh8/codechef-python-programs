f78b0cf374ef80222075580741be1d408dfdf897
Surya Pratap Singh <117995172+suryapratapsingh8@users.noreply.github.com>
2023-07-18 10:55:41 +0530

    Add files via upload

---
+++ WHICHDIV.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    r=int(input())
+    if r>=2000:
+        print(1)
+    elif r<2000 and r>=1600:
+        print(2)
+    else:
+        print(3)
