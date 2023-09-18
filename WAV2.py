61482fd30cc02560660efbd478a85a3938e3b094
Surya Pratap Singh <117995172+suryapratapsingh8@users.noreply.github.com>
2023-09-18 21:38:15 +0530

    Add files via upload

---
+++ WAV2.py
@@ -0,0 +1,13 @@
+import bisect
+N, Q = map(int, input().split())
+B = sorted([int(x) for x in input().split()])
+for _ in range(Q):
+    x = int(input())
+    c = bisect.bisect_left(B, x)
+    if c < N and B[c] == x:
+        print("0")
+    elif c == N or (N - 1 - c) %2 != 0:
+        print("POSITIVE")
+    else:
+        print("NEGATIVE")
+    
